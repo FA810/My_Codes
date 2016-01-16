48c346a97e030f87da7a33657a69aab29f3a0f66
FA810 <fabio.rizzello@tiscali.it>
2016-01-16 02:14:28 +0100

    the client server gaming  simulation  has been Mavenized

---
+++ Java/clientServer/terminals.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mate-terminal --working-directory=/home/fabio/workspace/clientServer --geometry=96x36+5+30
+mate-terminal --working-directory=/home/fabio/workspace/clientServer --geometry=68x17-5+30
+mate-terminal --working-directory=/home/fabio/workspace/clientServer --geometry=68x17-5+350
