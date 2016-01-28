d44e7407d4eda3cb227aa761e6c4d2b57ff6460a
FA810 <fabio.rizzello@tiscali.it>
2016-01-28 22:17:48 +0100

    some replacements of directories

--- Java/clientServerGaming/terminals.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mate-terminal --working-directory=/home/fabio/workspace/clientServer --geometry=96x36+5+30
-mate-terminal --working-directory=/home/fabio/workspace/clientServer --geometry=68x17-5+30
-mate-terminal --working-directory=/home/fabio/workspace/clientServer --geometry=68x17-5+350
