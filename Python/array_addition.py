b7c40c0fefdb75e86e76ca035bb802a324417e0a
FA810 <fabio.rizzello@tiscali.it>
2016-02-14 00:24:57 +0100

    comments

--- Python/array_addition.py
+++ Python/array_addition.py
@@ -1,3 +1,11 @@
+'''
+Have the function ArrayAdditionI(arr) take the array of numbers stored in arr 
+and return the string true if any combination of numbers in the array can be 
+added up to equal the largest number in the array, otherwise return the string 
+false. For example: if arr contains [4, 6, 23, 10, 1, 3] the output should 
+return true because 4 + 6 + 10 + 3 = 23. The array will not be empty, will not 
+contain all the same elements, and may contain negative numbers. 
+'''
 def ArrayAdditionI(arr):
 	arr = sorted(arr)
 	largest = arr.pop()
@@ -15,5 +23,8 @@ def recursion(target,array):
     	return res
 
 
-arr = [4,8,1,17,19]
+arr = [4,8,1,17,19]		# return false
 print ArrayAdditionI(arr)
+
+arr2 = [4,6,23,10,1,3]	#retrun true
+print ArrayAdditionI(arr2)
