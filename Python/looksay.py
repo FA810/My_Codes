a52d748fd05a0c69a2934c32e9b10de8e5a30c5f
FA810 <fabio.rizzello@tiscali.it>
2016-02-16 00:10:00 +0100

    comment look and say sequence

--- Python/looksay.py
+++ Python/looksay.py
@@ -1,3 +1,14 @@
+'''
+Have the function LookSaySequence(num) take the num parameter being passed and 
+return the next number in the sequence according to the following rule: to 
+generate the next number in a sequence read off the digits of the given number, 
+counting the number of digits in groups of the same digit. For example, the 
+sequence beginning with 1 would be: 1, 11, 21, 1211, ... 
+The 11 comes from there being "one 1" before it and the 21 comes from there 
+being "two 1's" before it. So your program should return the next number in the 
+sequence given num. 
+'''
+
 def find_sequence(sen):
 	j = 0
 	while(j+1 < len(sen) and sen[j] == sen[j+1]):
@@ -18,8 +29,6 @@ def LookSaySequence(num):
     
 
 num = 2466   
-# keep this function call here  
-# to see how to enter arguments in Python scroll down
 print LookSaySequence(num)  
 
 
