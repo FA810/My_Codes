0a26ab1d4622cf67a4de80799ed3bc0bb3d1a961
FA810 <fabio.rizzello@tiscali.it>
2016-02-18 00:36:24 +0100

    better comments

--- Python/most_free_time.py
+++ Python/most_free_time.py
@@ -1,3 +1,18 @@
+'''
+Have the function MostFreeTime(strArr) read the strArr parameter being passed 
+which will represent a full day and will be filled with events that span from 
+time X to time Y in the day. The format of each event will be 
+hh:mmAM/PM-hh:mmAM/PM. For example, strArr may be ["10:00AM-12:30PM",
+"02:00PM-02:45PM","09:10AM-09:50AM"]. Your program will have to output the 
+longest amount of free time available between the start of your first event and 
+the end of your last event in the format: hh:mm. The start event should be the 
+earliest event in the day and the latest event should be the latest event in 
+the day. The output for the previous input would therefore be 01:30 (with the 
+earliest event in the day starting at 09:10AM and the latest event ending at 
+02:45PM). The input will contain at least 3 events and the events may be out 
+of order. 
+'''
+
 def min_bef(hh,mm,pm):
 	if (pm==0) and hh==12:
 		return 1440 
@@ -86,8 +101,6 @@ def MostFreeTime(times):
     
     
 sen = ["12:15PM-02:00PM","09:00AM-12:11PM","02:02PM-04:00PM"]
-# keep this function call here  
-# to see how to enter arguments in Python scroll down
 print MostFreeTime(sen)  
 
 
