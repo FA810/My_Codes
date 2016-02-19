75af60b42fd46a6c91be753ab004f3e3d08e3ec1
FA810 <fabio.rizzello@tiscali.it>
2016-02-19 21:55:25 +0100

    better comments

--- Python/multiply_persistence.py
+++ Python/multiply_persistence.py
@@ -1,3 +1,12 @@
+'''
+Have the function MultiplicativePersistence(num) take the num parameter being 
+passed which will always be a positive integer and return its multiplicative 
+persistence which is the number of times you must multiply the digits in num 
+until you reach a single digit. For example: if num is 39 then your program 
+should return 3 because 3 * 9 = 27 then 2 * 7 = 14 and finally 1 * 4 = 4 and 
+you stop at 4.
+'''
+
 def MultiplicativePersistence(num): 
 	if num < 10:
 		return 0
@@ -7,11 +16,7 @@ def MultiplicativePersistence(num):
 		for s in nus:
 			tmp *= int(s)
 		#print tmp	
-		return 1+MultiplicativePersistence(tmp)
-
-    
+		return 1+MultiplicativePersistence(tmp)    
     
 num = 25
-# keep this function call here  
-# to see how to enter arguments in Python scroll down
 print MultiplicativePersistence(num)  
